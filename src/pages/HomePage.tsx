4f1d04c196c7abe78262275d00b1c77d9c0370ec
liliaaaaaGit <lilia.schraut@icloud.com>
2025-11-10 02:02:12 +0100

    Fix incorrect App.tsx import in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,13 +2,3 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
-try {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-} catch (error) {
-  console.error('Failed to render app:', error)
-}
