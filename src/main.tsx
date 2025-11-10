8a5ccf6d395c7b1f230bfaa649abfe6913689b1f
liliaaaaaGit <lilia.schraut@icloud.com>
2025-11-10 01:59:49 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-  } catch (error) {
-    console.error('Failed to render app:', error)
-  }
+try {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+} catch (error) {
+  console.error('Failed to render app:', error)
 }
