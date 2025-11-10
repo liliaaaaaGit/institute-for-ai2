cb6172376a660626c85c046df4c60908805b5355
liliaaaaaGit <lilia.schraut@icloud.com>
2025-11-10 02:01:48 +0100

    Updated comparisons.ts

--- src/lib/comparisons.ts
+++ src/lib/comparisons.ts
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+function calculateEmissions() {
+  try {
     // if (email) {
     //   await logLeadEvent(sessionId, email, { modelId, tokens: tokensInput, co2Grams });
     // }
